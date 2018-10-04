938efb9324119589142ad8f6236f16eac8917101
NastyaLo <loyko2802@gmail.com>
2018-10-04 14:02:11 +0300

    Calculating length of a longest consecutive sequence

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
+    if (array.length < 2) return array.length;
+
+    array.sort((a, b) => a - b);
+
+    let maxLength = 1;
+    let localMaxLength = 1;
+
+    for (let i = 1; i < array.length; i++) {
+        if (array[i] == ( array[i - 1] + 1 )) {
+            localMaxLength++;
+        } else {
+            if (localMaxLength > maxLength) {
+                maxLength = localMaxLength;
+            }
+                localMaxLength = 1;
+        }
+    }
+
+    return maxLength;
 }
